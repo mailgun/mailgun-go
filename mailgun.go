0d6ce2b301b51432542e47a240d003fbccd14454
Michael Banzon <michael@banzon.dk>
2013-12-25 16:24:23 +0100

    Added domain to Mailgun type.

--- mailgun.go
+++ mailgun.go
@@ -1,17 +1,27 @@
 package mailgun
 
+const (
+	MAILGUN_API_ADDRESS = "https://api.mailgun.net/v2/samples.mailgun.org/messages"
+)
+
 type Mailgun interface {
+	Domain() string
 	ApiKey() string
 	SendMessage(m *MailgunMessage)
 }
 
 type mailgunImpl struct {
+	domain string
 	apiKey string
 }
 
-func NewMailgun(apiKey string) Mailgun {
-	m := mailgunImpl{apiKey: apiKey}
+func NewMailgun(domain, apiKey string) Mailgun {
+	m := mailgunImpl{domain: domain, apiKey: apiKey}
 	return &m
+}
+
+func (m *mailgunImpl) Domain() string {
+	return m.domain
 }
 
 func (m *mailgunImpl) ApiKey() string {
--- mailgun_test.go
+++ mailgun_test.go
@@ -4,10 +4,11 @@ import (
 	"testing"
 )
 
+const DOMAIN = "banzon.dk"
 const API_KEY = "testingapikey"
 
 func TestMailgun(t *testing.T) {
-	m := NewMailgun(API_KEY)
+	m := NewMailgun(DOMAIN, API_KEY)
 	if API_KEY != m.ApiKey() {
 		t.Fatal("ApiKey not equal!")
 	}
