a973fda4b13d28c1249d7dfdb746a95cdce955fd
Michael Banzon <michael@banzon.dk>
2013-12-26 14:03:36 +0100

    Sending mail

--- mailgun.go
+++ mailgun.go
@@ -1,8 +1,10 @@
 package mailgun
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
+	"net/url"
 )
 
 const (
@@ -36,13 +38,44 @@ func (m *mailgunImpl) ApiKey() string {
 }
 
 func (m *mailgunImpl) SendMessage(message *MailgunMessage) error {
-	// TODO
+	if !message.validateMessage() {
+		return errors.New("Message not valid")
+	}
+
 	req, err := http.NewRequest("POST", generateApiUrl(m, messagesEndpoint), nil)
 	if err != nil {
 		return err
 	}
 
 	req.SetBasicAuth(basicAuthUser, m.ApiKey())
+
+	data := url.Values{}
+	data.Add("from", message.From.String())
+	data.Add("subject", message.Subject)
+	data.Add("text", message.Text)
+
+	for _, to := range message.To {
+		data.Add("to", to.String())
+	}
+	for _, cc := range message.Cc {
+		data.Add("cc", cc.String())
+	}
+	for _, bcc := range message.Bcc {
+		data.Add("bcc", bcc.String())
+	}
+
+	if message.Html != "" {
+		data.Add("html", message.Html)
+	}
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	if resp.StatusCode != http.StatusOK {
+		return errors.New("Status is not 200")
+	}
 
 	return nil
 }
