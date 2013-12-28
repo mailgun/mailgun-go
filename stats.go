881ba4690b635f064f66c59761301c46619de3cf
Michael Banzon <michael@banzon.dk>
2013-12-28 11:03:04 +0100

    Added field to the struct. Added envelope for struct.

--- stats.go
+++ stats.go
@@ -1,4 +1,14 @@
 package mailgun
 
 type Stat struct {
+	Event      string `json:"event"`
+	TotalCount int    `json:"total_count"`
+	CreatedAt  string `json:"created_at"`
+	Id         string `json:"id"`
+	Tags       string `json:"tags"`
+}
+
+type statsEnvelope struct {
+	TotalCount int    `json:"total_count"`
+	Items      []Stat `json:"items"`
 }
