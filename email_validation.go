82d85519c27ea6c9bf3587c06ce0e99de582a286
Michael Banzon <michael@banzon.dk>
2013-12-29 15:25:50 +0100

    E-mail validation now uses simplehttp.

--- email_validation.go
+++ email_validation.go
@@ -1,13 +1,8 @@
 package mailgun
 
 import (
-	"encoding/json"
-	"errors"
-	"fmt"
-	"io/ioutil"
-	"net/http"
-	"net/url"
 	"strings"
+	"github.com/mbanzon/simplehttp"
 )
 
 type EmailVarificationParts struct {
@@ -29,80 +24,28 @@ type AddressParseResult struct {
 }
 
 func (m *mailgunImpl) ValidateEmail(email string) (EmailVerification, error) {
-	u, err := url.Parse(generatePublicApiUrl(addressValidateEndpoint))
-	if err != nil {
-		return EmailVerification{}, err
-	}
-	q := u.Query()
-	q.Set("address", email)
-	u.RawQuery = q.Encode()
-
-	req, err := http.NewRequest("GET", u.String(), nil)
-
-	if err != nil {
-		return EmailVerification{}, err
-	}
-	req.SetBasicAuth(basicAuthUser, m.PublicApiKey())
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		return EmailVerification{}, err
-	}
-	if resp.StatusCode != http.StatusOK {
-		return EmailVerification{}, errors.New(fmt.Sprintf("Status is not 200. It was %d", resp.StatusCode))
-	}
-
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return EmailVerification{}, err
-	}
+	r := simplehttp.NewSimpleHTTPRequest("GET", generatePublicApiUrl(addressValidateEndpoint))
+	r.AddParameter("address", email)
+	r.SetBasicAuth(basicAuthUser, m.PublicApiKey())
 
 	var response EmailVerification
-	err2 := json.Unmarshal(body, &response)
-	if err2 != nil {
-		return EmailVerification{}, err2
+	err := r.MakeJSONRequest(&response)
+	if err != nil {
+		return EmailVerification{}, err
 	}
 
 	return response, nil
 }
 
 func (m *mailgunImpl) ParseAddresses(addresses ...string) ([]string, []string, error) {
-	u, err := url.Parse(generatePublicApiUrl(addressParseEndpoint))
-	if err != nil {
-		return nil, nil, err
-	}
-	q := u.Query()
-	q.Set("addresses", strings.Join(addresses, ","))
-	u.RawQuery = q.Encode()
-
-	req, err := http.NewRequest("GET", u.String(), nil)
-
-	if err != nil {
-		return nil, nil, err
-	}
-	req.SetBasicAuth(basicAuthUser, m.PublicApiKey())
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		return nil, nil, err
-	}
-	if resp.StatusCode != http.StatusOK {
-		return nil, nil, errors.New(fmt.Sprintf("Status is not 200. It was %d", resp.StatusCode))
-	}
-
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return nil, nil, err
-	}
+	r := simplehttp.NewSimpleHTTPRequest("GET", generatePublicApiUrl(addressParseEndpoint))
+	r.AddParameter("addresses", strings.Join(addresses, ","))
+	r.SetBasicAuth(basicAuthUser, m.PublicApiKey())
 
 	var response AddressParseResult
-	err2 := json.Unmarshal(body, &response)
-	if err2 != nil {
-		return nil, nil, err2
+	err := r.MakeJSONRequest(&response)
+	if err != nil {
+		return nil, nil, err
 	}
 
 	return response.Parsed, response.Unparseable, nil
