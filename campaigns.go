ecb6dfdece9a02a114df200b6bce549b302ec4b7
Michael Banzon <michael@banzon.dk>
2014-01-04 11:42:09 +0100

    Added JSON-names and did gofmt.

--- campaigns.go
+++ campaigns.go
@@ -1,15 +1,15 @@
 package mailgun
 
 type Campaign struct {
-	Id                string
-	Name              string
-	CreatedAt         string
-	DeliveredCount    int
-	ClickedCount      int
-	OpenedCount       int
-	SubmittedCount    int
-	UnsubscribedCount int
-	BouncedCount      int
-	ComplainedCount   int
-	DroppedCount      int
+	Id                string `json:"id"`
+	Name              string `json:"name"`
+	CreatedAt         string `json:"created_at"`
+	DeliveredCount    int    `json:"delivered_count"`
+	ClickedCount      int    `json:"clicked_count"`
+	OpenedCount       int    `json:"opened_count"`
+	SubmittedCount    int    `json:"submitted_count"`
+	UnsubscribedCount int    `json:"unsubscribed_count"`
+	BouncedCount      int    `json:"bounced_count"`
+	ComplainedCount   int    `json:"complained_count"`
+	DroppedCount      int    `json:"dropped_count"`
 }
