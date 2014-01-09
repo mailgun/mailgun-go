c8f3a50a4cb195edfcdbbebd3385a15458f55de2
Michael Banzon <michael@banzon.dk>
2014-01-09 21:20:30 +0100

    Added more to the complaint api.

--- spam_complaints.go
+++ spam_complaints.go
@@ -1,12 +1,26 @@
 package mailgun
 
+import (
+	"github.com/mbanzon/simplehttp"
+)
+
+const (
+	complaintsEndpoint = "complaints"
+)
+
 type Complaint struct {
-	Count     int
-	CreatedAt string
-	Address   string
+	Count     int    `json:"count"`
+	CreatedAt string `json:"created_at"`
+	Address   string `json:"address"`
+}
+
+type complaintContainer struct {
+	TotalCount int         `json:"total_count"`
+	Items      []Complaint `json:"items"`
 }
 
 func (m *mailgunImpl) GetComplaints(limit, skip int) (int, []interface{}, error) {
+	simplehttp.NewGetRequest(generateApiUrl(m, complaintsEndpoint))
 	// TODO - this is NOT complete!
 	return -1, nil, nil
 }
