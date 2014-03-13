5fd71675078e9dd9b976789a8c693afc40743e5d
Samuel A. Falvo II <sam.falvo@rackspace.com>
2014-03-13 15:48:39 -0700

    Add get specific comaplaint test

--- acceptance/spam_complaints_test.go
+++ acceptance/spam_complaints_test.go
@@ -20,3 +20,17 @@ func TestGetComplaints(t *testing.T) {
 		t.Fatalf("Expected %d complaints; got %d", n, len(complaints))
 	}
 }
+
+func TestGetComplaintFromBazNoComplaint(t *testing.T) {
+	domain := reqEnv(t, "MG_DOMAIN")
+	apiKey := reqEnv(t, "MG_API_KEY")
+	publicApiKey := reqEnv(t, "MG_PUBLIC_API_KEY")
+	mg := mailgun.NewMailgun(domain, apiKey, publicApiKey)
+	complaint, err := mg.GetSingleComplaint("baz@example.com")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if complaint.Count != 0 {
+		t.Fatal("Expected baz@example.com to have zero complaints.")
+	}
+}
