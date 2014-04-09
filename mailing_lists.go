50e7a8f90c6c0a4d51988f8d23f7e78e23519221
Samuel A. Falvo II <sam.falvo@rackspace.com>
2014-04-09 10:12:30 -0700

    Add godocs.

--- mailing_lists.go
+++ mailing_lists.go
@@ -7,25 +7,42 @@ import (
 //	"fmt"
 )
 
+// A mailing list may have one of three membership modes.
+// ReadOnly specifies that nobody, including subscribers,
+// may send messages to the mailing list.
+// Messages distributed on such lists come from list administrator accounts only.
+// Members specifies that only those who subscribe to the mailing list may send messages.
+// Everyone specifies that anyone and everyone may both read and submit messages
+// to the mailing list, including non-subscribers.
 const (
 	ReadOnly = "readonly"
 	Members = "members"
 	Everyone = "everyone"
 )
 
-type Subscription string
-
+// Mailing list members have an attribute that determines if they've subscribed to the mailing list or not.
+// This attribute may be used to filter the results returned by GetSubscribers().
+// All, Subscribed, and Unsubscribed provides a convenient and readable syntax for specifying the scope of the search.
 var (
 	All *bool = nil
 	Subscribed *bool = &yes
 	Unsubscribed *bool = &no
 )
 
+// yes and no are variables which provide us the ability to take their addresses.
+// Subscribed and Unsubscribed are pointers to these booleans.
+//
+// We use a pointer to boolean as a kind of trinary data type:
+// if nil, the relevant data type remains unspecified.
+// Otherwise, its value is either true or false.
 var (
 	yes bool = true
 	no bool = false
 )
 
+// A List structure provides information for a mailing list.
+//
+// AccessLevel may be one of ReadOnly, Members, or Everyone.
 type List struct {
 	Address      string `json:"address",omitempty"`
 	Name         string `json:"name",omitempty"`
@@ -35,6 +52,8 @@ type List struct {
 	MembersCount int    `json:"members_count",omitempty"`
 }
 
+// A Subscriber structure represents a member of the mailing list.
+// The Vars field can represent any JSON-encodable data.
 type Subscriber struct {
 	Address    string `json:"address,omitempty"`
 	Name       string `json:"name,omitempty"`
@@ -42,6 +61,7 @@ type Subscriber struct {
 	Vars       map[string]interface{} `json:"vars,omitempty"`
 }
 
+// GetLists returns the specified set of mailing lists administered by your account.
 func (mg *mailgunImpl) GetLists(limit, skip int, filter string) (int, []List, error) {
 	r := simplehttp.NewHTTPRequest(generatePublicApiUrl(listsEndpoint))
 	r.SetBasicAuth(basicAuthUser, mg.ApiKey())
@@ -67,6 +87,11 @@ func (mg *mailgunImpl) GetLists(limit, skip int, filter string) (int, []List, er
 	return envelope.TotalCount, envelope.Items, err
 }
 
+// CreateList creates a new mailing list under your Mailgun account.
+// You need specify only the Address and Name members of the prototype;
+// Description, and AccessLevel are optional.
+// If unspecified, Description remains blank,
+// while AccessLevel defaults to Everyone.
 func (mg *mailgunImpl) CreateList(prototype List) (List, error) {
 	r := simplehttp.NewHTTPRequest(generatePublicApiUrl(listsEndpoint))
 	r.SetBasicAuth(basicAuthUser, mg.ApiKey())
@@ -92,6 +117,8 @@ func (mg *mailgunImpl) CreateList(prototype List) (List, error) {
 	return l, err
 }
 
+// DeleteList removes all current members of the list, then removes the list itself.
+// Attempts to send e-mail to the list will fail subsequent to this call.
 func (mg *mailgunImpl) DeleteList(addr string) error {
 	r := simplehttp.NewHTTPRequest(generatePublicApiUrl(listsEndpoint) + "/" + addr)
 	r.SetBasicAuth(basicAuthUser, mg.ApiKey())
@@ -99,6 +126,8 @@ func (mg *mailgunImpl) DeleteList(addr string) error {
 	return err
 }
 
+// GetListByAddress allows your application to recover the complete List structure
+// representing a mailing list, so long as you have its e-mail address.
 func (mg *mailgunImpl) GetListByAddress(addr string) (List, error) {
 	r := simplehttp.NewHTTPRequest(generatePublicApiUrl(listsEndpoint) + "/" + addr)
 	r.SetBasicAuth(basicAuthUser, mg.ApiKey())
@@ -110,6 +139,13 @@ func (mg *mailgunImpl) GetListByAddress(addr string) (List, error) {
 	return envelope.List, err
 }
 
+// UpdateList allows you to change various attributes of a list.
+// Address, Name, Description, and AccessLevel are all optional;
+// only those fields which are set in the prototype will change.
+//
+// Be careful!  If changing the address of a mailing list,
+// e-mail sent to the old address will not succeed.
+// Make sure you account for the change accordingly.
 func (mg *mailgunImpl) UpdateList(addr string, prototype List) (List, error) {
 	r := simplehttp.NewHTTPRequest(generatePublicApiUrl(listsEndpoint) + "/" + addr)
 	r.SetBasicAuth(basicAuthUser, mg.ApiKey())
@@ -135,7 +171,10 @@ func (mg *mailgunImpl) UpdateList(addr string, prototype List) (List, error) {
 	return l, err
 }
 
-
+// GetSubscribers returns the list of members belonging to the indicated mailing list.
+// The s parameter can be set to one of three settings to help narrow the returned data set:
+// All indicates that you want both subscribers and unsubscribed members alike, while
+// Subscribed and Unsubscribed indicate you want only those eponymous subsets.
 func (mg *mailgunImpl) GetSubscribers(limit, skip int, s *bool, addr string) (int, []Subscriber, error) {
 	r := simplehttp.NewHTTPRequest(generateSubscriberApiUrl(listsEndpoint, addr))
 	r.SetBasicAuth(basicAuthUser, mg.ApiKey())
@@ -161,6 +200,8 @@ func (mg *mailgunImpl) GetSubscribers(limit, skip int, s *bool, addr string) (in
 	return envelope.TotalCount, envelope.Items, err
 }
 
+// GetSubscriberByAddress returns a complete Subscriber structure for a member of a mailing list,
+// given only their subscription e-mail address.
 func (mg *mailgunImpl) GetSubscriberByAddress(s, l string) (Subscriber, error) {
 	r := simplehttp.NewHTTPRequest(generateSubscriberApiUrl(listsEndpoint, l) + "/" + s)
 	r.SetBasicAuth(basicAuthUser, mg.ApiKey())
@@ -175,6 +216,9 @@ func (mg *mailgunImpl) GetSubscriberByAddress(s, l string) (Subscriber, error) {
 	return envelope.Member, err
 }
 
+// CreateSubscriber registers a new member of the indicated mailing list.
+// If merge is set to true, then the registration may update an existing subscriber's settings.
+// Otherwise, an error will occur if you attempt to add a member with a duplicate e-mail address.
 func (mg *mailgunImpl) CreateSubscriber(merge bool, addr string, prototype Subscriber) error {
 	vs, err := json.Marshal(prototype.Vars)
 	if err != nil {
@@ -195,6 +239,8 @@ func (mg *mailgunImpl) CreateSubscriber(merge bool, addr string, prototype Subsc
 	return err
 }
 
+// UpdateSubscriber lets you change certain details about the indicated mailing list member.
+// Address, Name, Vars, and Subscribed fields may be changed.
 func (mg *mailgunImpl) UpdateSubscriber(s, l string, prototype Subscriber) (Subscriber, error) {
 	r := simplehttp.NewHTTPRequest(generateSubscriberApiUrl(listsEndpoint, l) + "/" + s)
 	r.SetBasicAuth(basicAuthUser, mg.ApiKey())
