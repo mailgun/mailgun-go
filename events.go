3c91947a8a7bc041fd184f353291ab948acde90d
Samuel A. Falvo II <sam.falvo@rackspace.com>
2014-05-08 11:41:57 -0700

    Replace GetEvents with NewEventIterator

--- events.go
+++ events.go
@@ -31,20 +31,28 @@ type GetEventsOptions struct {
 	Filter                                   map[string]string
 }
 
+// EventIterator maintains the state necessary for paging though small parcels of a larger set of events.
 type EventIterator struct {
 	events []Event
 	nextURL, prevURL string
 	mg Mailgun
 }
 
+// NewEventIterator creates a new iterator for events.
+// Use GetFirstPage to retrieve the first batch of events.
+// Use Next and Previous thereafter as appropriate to iterate through sets of data.
 func (mg *MailgunImpl) NewEventIterator() *EventIterator {
 	return &EventIterator{mg: mg}
 }
 
+// Events returns the most recently retrieved batch of events.
+// The length is guaranteed to fall between 0 and the limit set in the GetEventsOptions structure passed to GetFirstPage.
 func (ei *EventIterator) Events() []Event {
 	return ei.events
 }
 
+// GetFirstPage retrieves the first batch of events, according to your criteria.
+// See the GetEventsOptions structure for more details on how the fields affect the data returned.
 func (ei *EventIterator) GetFirstPage(opts GetEventsOptions) error {
 	if opts.ForceAscending && opts.ForceDescending {
 		return fmt.Errorf("collation cannot at once be both ascending and descending")
@@ -82,14 +90,20 @@ func (ei *EventIterator) GetFirstPage(opts GetEventsOptions) error {
 	return ei.fetch(url)
 }
 
+// Retrieves the chronologically previous batch of events, if any exist.
+// You know you're at the end of the list when len(Events())==0.
 func (ei *EventIterator) GetPrevious() error {
 	return ei.fetch(ei.prevURL)
 }
 
+// Retrieves the chronologically next batch of events, if any exist.
+// You know you're at the end of the list when len(Events())==0.
 func (ei *EventIterator) GetNext() error {
 	return ei.fetch(ei.nextURL)
 }
 
+// GetFirstPage, GetPrevious, and GetNext all have a common body of code.
+// fetch completes the API fetch common to all three of these functions.
 func (ei *EventIterator) fetch(url string) error {
 	r := simplehttp.NewHTTPRequest(url)
 	r.SetBasicAuth(basicAuthUser, ei.mg.ApiKey())
