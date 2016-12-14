992c089dcbfc5be00b26d0c393166fe353210712
Derrick J. Wippler <thrawn01@gmail.com>
2016-12-14 17:18:34 -0600

    TestEventIterator should now pass consistently

--- events_test.go
+++ events_test.go
@@ -12,7 +12,7 @@ func TestEventIterator(t *testing.T) {
 
 	// Grab the list of events (as many as we can get)
 	ei := mg.NewEventIterator()
-	err = ei.GetFirstPage(GetEventsOptions{})
+	err = ei.GetFirstPage(GetEventsOptions{ForceDescending: true})
 	if err != nil {
 		t.Fatal(err)
 	}
