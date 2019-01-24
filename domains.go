9ad4bbff1252a537dc427b8dab5947c99252e0b1
Shaumyadeep Chaudhuri <shaumya@gmail.com>
2019-01-24 14:19:56 +0530

    Verify Domain API implementation

--- domains.go
+++ domains.go
@@ -237,6 +237,17 @@ func (mg *MailgunImpl) GetDomain(ctx context.Context, domain string) (Domain, []
 	return resp.Domain, resp.ReceivingDNSRecords, resp.SendingDNSRecords, err
 }
 
+func (mg *MailgunImpl) VerifyDomain(ctx context.Context, domain string) (string, error) {
+	r := newHTTPRequest(generatePublicApiUrl(mg, domainsEndpoint) + "/" + domain + "/verify")
+	r.setClient(mg.Client())
+	r.setBasicAuth(basicAuthUser, mg.APIKey())
+
+	payload := newUrlEncodedPayload()
+	var resp domainResponse
+	err := putResponseFromJSON(ctx, r, payload, &resp)
+	return resp.Domain.State, err
+}
+
 // Optional parameters when creating a domain
 type CreateDomainOptions struct {
 	SpamAction         SpamAction
