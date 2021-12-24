429fa9e5e780430b08e05d82de9d39e71cc25ecc
Tomash Sidei <tsidei@softserveinc.com>
2021-12-24 13:53:17 +0200

    Add credentials mock and use it for credentials tests.

--- credentials_test.go
+++ credentials_test.go
@@ -1,27 +1,24 @@
-package mailgun
+package mailgun_test
 
 import (
 	"context"
 	"fmt"
-	"os"
 	"strings"
 	"testing"
+
+	"github.com/mailgun/mailgun-go/v4"
 
 	"github.com/facebookgo/ensure"
 )
 
 func TestGetCredentials(t *testing.T) {
-	if reason := SkipNetworkTest(); reason != "" {
-		t.Skip(reason)
-	}
-
-	mg, err := NewMailgunFromEnv()
-	ensure.Nil(t, err)
+	mg := mailgun.NewMailgun(testDomain, testKey)
+	mg.SetAPIBase(server.URL())
 
 	ctx := context.Background()
 	it := mg.ListCredentials(nil)
 
-	var page []Credential
+	var page []mailgun.Credential
 	for it.Next(ctx, &page) {
 		t.Logf("Login\tCreated At\t\n")
 		for _, c := range page {
@@ -32,17 +29,12 @@ func TestGetCredentials(t *testing.T) {
 }
 
 func TestCreateDeleteCredentials(t *testing.T) {
-	if reason := SkipNetworkTest(); reason != "" {
-		t.Skip(reason)
-	}
-
-	domain := os.Getenv("MG_DOMAIN")
-	mg, err := NewMailgunFromEnv()
-	ensure.Nil(t, err)
+	mg := mailgun.NewMailgun(testDomain, testKey)
+	mg.SetAPIBase(server.URL())
 
 	randomPassword := randomString(16, "pw")
 	randomID := strings.ToLower(randomString(16, "usr"))
-	randomLogin := fmt.Sprintf("%s@%s", randomID, domain)
+	randomLogin := fmt.Sprintf("%s@%s", randomID, testDomain)
 
 	ctx := context.Background()
 	ensure.Nil(t, mg.CreateCredential(ctx, randomLogin, randomPassword))
