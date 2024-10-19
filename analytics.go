4ded58f34073490f041c7858053c09670b1061c5
Vilen Topchii <32271530+vtopc@users.noreply.github.com>
2024-10-19 18:02:57 +0300

    added iterator

--- analytics.go
+++ analytics.go
@@ -13,7 +13,7 @@ import (
 // as `https://api.mailgun.net/v1` or set `mg.SetAPIBase("https://api.mailgun.net/v1")`
 //
 // https://documentation.mailgun.com/docs/mailgun/api-reference/openapi-final/tag/Metrics/
-func (mg *MailgunImpl) ListMetrics(ctx context.Context, opts MetricsOptions) (*MetricsResponse, error) {
+func (mg *MailgunImpl) ListMetrics(opts MetricsOptions) (*MetricsIterator, error) {
 	if !strings.HasSuffix(mg.APIBase(), "/v1") {
 		return nil, errors.New("only v1 API is supported")
 	}
@@ -29,24 +29,18 @@ func (mg *MailgunImpl) ListMetrics(ctx context.Context, opts MetricsOptions) (*M
 		opts.Filter.BoolGroupAnd = append(opts.Filter.BoolGroupAnd, domainFilter)
 	}
 
+	if opts.Pagination.Limit == 0 {
+		opts.Pagination.Limit = 10
+	}
+
 	req := newHTTPRequest(generatePublicApiUrl(mg, metricsEndpoint))
 	req.setClient(mg.Client())
 	req.setBasicAuth(basicAuthUser, mg.APIKey())
 
-	payload := newJSONEncodedPayload(opts)
-
-	resp, err := makePostRequest(ctx, req, payload)
-	if err != nil {
-		return nil, errors.Errorf("POST %s failed: %s", metricsEndpoint, err)
-	}
-
-	var ret MetricsResponse
-	err = resp.parseFromJSON(&ret)
-	if err != nil {
-		return nil, errors.Wrap(err, "decoding response")
-	}
-
-	return &ret, nil
+	return &MetricsIterator{
+		opts: opts,
+		req:  req,
+	}, nil
 }
 
 type MetricsPagination struct {
@@ -58,4 +52,56 @@ type MetricsPagination struct {
 	Limit int64 `json:"limit"`
 	// The total number of items in the query result set.
 	Total int64 `json:"total"`
+}
+
+type MetricsIterator struct {
+	opts MetricsOptions
+	req  *httpRequest
+	resp MetricsResponse
+	err  error
+}
+
+func (iter *MetricsIterator) Err() error {
+	return iter.err
+}
+
+// Next retrieves the next page of items from the api. Returns false when there are
+// no more pages to retrieve or if there was an error.
+// Use `.Err()` to retrieve the error
+func (iter *MetricsIterator) Next(ctx context.Context, resp *MetricsResponse) bool {
+	if iter.err != nil {
+		return false
+	}
+
+	iter.err = iter.fetch(ctx)
+	if iter.err != nil {
+		return false
+	}
+
+	*resp = iter.resp
+	if len(iter.resp.Items) == 0 {
+		return false
+	}
+	iter.opts.Pagination.Skip = iter.opts.Pagination.Skip + iter.opts.Pagination.Limit
+
+	return true
+}
+
+func (iter *MetricsIterator) fetch(ctx context.Context) error {
+	payload := newJSONEncodedPayload(iter.opts)
+
+	httpResp, err := makePostRequest(ctx, iter.req, payload)
+	if err != nil {
+		return errors.Errorf("POST %s failed: %s", metricsEndpoint, err)
+	}
+
+	var resp MetricsResponse
+	err = httpResp.parseFromJSON(&resp)
+	if err != nil {
+		return errors.Wrap(err, "decoding response")
+	}
+
+	iter.resp = resp
+
+	return nil
 }
