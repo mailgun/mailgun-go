b4a15e8b934b373239ab3ebea728d68f6df6da5b
Vilen Topchii <32271530+vtopc@users.noreply.github.com>
2024-10-21 19:51:51 +0300

    switched to integration test

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/go-chi/chi/v5 v5.0.8
 	github.com/json-iterator/go v1.1.10
 	github.com/mailgun/errors v0.3.0
+	github.com/stretchr/testify v1.9.0
 )
 
 require (
@@ -15,6 +16,8 @@ require (
 	github.com/facebookgo/subset v0.0.0-20150612182917-8dac2c3c4870 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
 	github.com/modern-go/reflect2 v0.0.0-20180701023420-4b7aa43c6742 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/sirupsen/logrus v1.9.0 // indirect
 	golang.org/x/sys v0.0.0-20220919091848-fb04ddd9f9c8 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ integration_test.go
@@ -0,0 +1,64 @@
+//go:build integration
+
+package mailgun_test
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/mailgun/mailgun-go/v4"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestIntegrationMailgunImpl_ListMetrics(t *testing.T) {
+	mg, err := mailgun.NewMailgunFromEnv()
+	if err != nil {
+		require.NoError(t, err)
+	}
+
+	opts := mailgun.MetricsOptions{
+		End:      mailgun.RFC2822Time(time.Now().UTC()),
+		Duration: "30d",
+		Pagination: mailgun.MetricsPagination{
+			Limit: 10,
+		},
+	}
+
+	iter, err := mg.ListMetrics(opts)
+	if err != nil {
+		require.NoError(t, err)
+	}
+
+	for i := 0; i < 2; i++ {
+		func() {
+			ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+			defer cancel()
+
+			resp := mailgun.MetricsResponse{}
+			more := iter.Next(ctx, &resp)
+			if iter.Err() != nil {
+				require.NoError(t, err)
+			}
+
+			t.Logf("Page %d: Start: %s; End: %s; Pagination: %+v\n",
+				i+1, resp.Start, resp.End, resp.Pagination)
+
+			assert.GreaterOrEqual(t, len(resp.Items), 1)
+
+			for _, item := range resp.Items {
+				b, _ := json.Marshal(item)
+				t.Logf("%s\n", b)
+			}
+
+			if !more {
+				log.Print("no more pages")
+				os.Exit(0)
+			}
+		}()
+	}
+}
