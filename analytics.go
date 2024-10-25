519cb672bd1db17608d72d39e0ce2a18ef8e4a1e
Vilen Topchii <32271530+vtopc@users.noreply.github.com>
2024-10-25 20:38:51 +0300

    sort

--- analytics.go
+++ analytics.go
@@ -7,6 +7,17 @@ import (
 	"github.com/mailgun/errors"
 )
 
+type MetricsPagination struct {
+	// Colon-separated value indicating column name and sort direction e.g. 'domain:asc'.
+	Sort string `json:"sort"`
+	// The number of items to skip over when satisfying the request. To get the first page of data set skip to zero. Then increment the skip by the limit for subsequent calls.
+	Skip int `json:"skip"`
+	// The maximum number of items returned in the response.
+	Limit int `json:"limit"`
+	// The total number of items in the query result set.
+	Total int `json:"total"`
+}
+
 // ListMetrics returns domain/account metrics.
 //
 // NOTE: Only for v1 API. To use the /v1 version define MG_URL in the environment variable
@@ -41,17 +52,6 @@ func (mg *MailgunImpl) ListMetrics(opts MetricsOptions) (*MetricsIterator, error
 		opts: opts,
 		req:  req,
 	}, nil
-}
-
-type MetricsPagination struct {
-	// Colon-separated value indicating column name and sort direction e.g. 'domain:asc'.
-	Sort string `json:"sort"`
-	// The number of items to skip over when satisfying the request. To get the first page of data set skip to zero. Then increment the skip by the limit for subsequent calls.
-	Skip int `json:"skip"`
-	// The maximum number of items returned in the response.
-	Limit int `json:"limit"`
-	// The total number of items in the query result set.
-	Total int `json:"total"`
 }
 
 type MetricsIterator struct {
