2d0f6f65a4d01a863474e95d78a9ce5c8f11e4a0
Vilen Topchii <32271530+vtopc@users.noreply.github.com>
2024-10-28 18:11:44 +0200

    fmt

--- analytics.go
+++ analytics.go
@@ -10,7 +10,9 @@ import (
 type MetricsPagination struct {
 	// Colon-separated value indicating column name and sort direction e.g. 'domain:asc'.
 	Sort string `json:"sort"`
-	// The number of items to skip over when satisfying the request. To get the first page of data set skip to zero. Then increment the skip by the limit for subsequent calls.
+	// The number of items to skip over when satisfying the request.
+	// To get the first page of data set skip to zero.
+	// Then increment the skip by the limit for subsequent calls.
 	Skip int `json:"skip"`
 	// The maximum number of items returned in the response.
 	Limit int `json:"limit"`
