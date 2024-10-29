eb6d3983b347018d4a43e74cbd2eb97025b848e2
Vilen Topchii <32271530+vtopc@users.noreply.github.com>
2024-10-29 13:56:05 +0200

    added mutexes

--- exports.go
+++ exports.go
@@ -83,7 +83,9 @@ func (mg *MailgunImpl) GetExportLink(ctx context.Context, id string) (string, er
 
 	if Debug {
 		if CaptureCurlOutput {
+			r.mu.Lock()
 			r.capturedCurlOutput = r.curlString(req, nil)
+			r.mu.Unlock()
 		} else {
 			fmt.Println(r.curlString(req, nil))
 		}
