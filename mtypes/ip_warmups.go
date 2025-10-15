f5c5472e551d36f379a380fbbea344cabf068c1e
Stephen Young <stephen@customer.io>
2025-10-15 16:17:54 -0400

    rfc3339 is the default

--- mtypes/ip_warmups.go
+++ mtypes/ip_warmups.go
@@ -1,5 +1,7 @@
 package mtypes
 
+import "time"
+
 type ListIPWarmupsResponse struct {
 	Paging Paging     `json:"paging"`
 	Items  []IPWarmup `json:"items"`
@@ -7,13 +9,13 @@ type ListIPWarmupsResponse struct {
 
 // IPWarmup - is a response for ListIPWarmups.
 type IPWarmup struct {
-	IP               string      `json:"ip"`
-	SentWithinStage  string      `json:"sent_within_stage"`
-	Throttle         int         `json:"throttle"`
-	StageNumber      int         `json:"stage_number"`
-	StageStartVolume int         `json:"stage_start_volume"`
-	StageStartTime   RFC3339Time `json:"stage_start_time"`
-	StageVolumeLimit int         `json:"stage_volume_limit"`
+	IP               string    `json:"ip"`
+	SentWithinStage  string    `json:"sent_within_stage"`
+	Throttle         int       `json:"throttle"`
+	StageNumber      int       `json:"stage_number"`
+	StageStartVolume int       `json:"stage_start_volume"`
+	StageStartTime   time.Time `json:"stage_start_time"`
+	StageVolumeLimit int       `json:"stage_volume_limit"`
 }
 
 type IPWarmupDetailsResponse struct {
@@ -25,18 +27,18 @@ type IPWarmupDetails struct {
 	Throttle          int                    `json:"throttle"`
 	StageNumber       int                    `json:"stage_number"`
 	StageStartVolume  int                    `json:"stage_start_volume"`
-	StageStartTime    RFC3339Time            `json:"stage_start_time"`
+	StageStartTime    time.Time              `json:"stage_start_time"`
 	StageVolumeLimit  int                    `json:"stage_volume_limit"`
-	StageStartedAt    RFC3339Time            `json:"stage_started_at"`
-	HourStartedAt     RFC3339Time            `json:"hour_started_at"`
-	PlanStartedAt     RFC3339Time            `json:"plan_started_at"`
-	PlanLastUpdatedAt RFC3339Time            `json:"plan_last_updated_at"`
+	StageStartedAt    time.Time              `json:"stage_started_at"`
+	HourStartedAt     time.Time              `json:"hour_started_at"`
+	PlanStartedAt     time.Time              `json:"plan_started_at"`
+	PlanLastUpdatedAt time.Time              `json:"plan_last_updated_at"`
 	TotalStages       int                    `json:"total_stages"`
 	StageHistory      []IPWarmupStageHistory `json:"stage_history"`
 }
 
 type IPWarmupStageHistory struct {
-	FirstUpdatedAt RFC3339Time `json:"first_updated_at"`
-	CompletedAt    RFC3339Time `json:"completed_at"`
-	Limit          int         `json:"limit"`
+	FirstUpdatedAt time.Time `json:"first_updated_at"`
+	CompletedAt    time.Time `json:"completed_at"`
+	Limit          int       `json:"limit"`
 }
