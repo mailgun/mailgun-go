62723dc00eaae9c0b2206e2d81db55379d03b330
Teko <112829523+Teko012@users.noreply.github.com>
2026-08-05 17:18:49 +0200

    Remove email provider lookup test case
    Removed the TestValidateEmailProviderLookupParameter function and its associated HTTP test server setup.

--- validation_test.go
+++ validation_test.go
@@ -2,9 +2,6 @@ package mailgun_test
 
 import (
 	"context"
-	"fmt"
-	"net/http"
-	"net/http/httptest"
 	"testing"
 
 	"github.com/mailgun/mailgun-go/v5"
@@ -51,26 +48,4 @@ func TestValidateEmail(t *testing.T) {
 		assert.False(t, ev.Engagement.Engaging)
 		assert.False(t, ev.Engagement.IsBot)
 	})
-}
-
-func TestValidateEmailProviderLookupParameter(t *testing.T) {
-	srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-		assert.Equal(t, http.MethodGet, req.Method)
-		assert.Equal(t, "/v4/address/validate", req.URL.Path)
-		assert.Equal(t, "foo@mailgun.com", req.FormValue("address"))
-		assert.Equal(t, "true", req.FormValue("provider_lookup"))
-		assert.Empty(t, req.FormValue("mailbox_verification"))
-		_, err := fmt.Fprint(w, `{"address":"foo@mailgun.com","result":"deliverable","risk":"low","reason":[]}`)
-		require.NoError(t, err)
-	}))
-	defer srv.Close()
-
-	mg := mailgun.NewMailgun(testKey)
-	err := mg.SetAPIBase(srv.URL)
-	require.NoError(t, err)
-
-	ctx := context.Background()
-
-	_, err = mg.ValidateEmail(ctx, "foo@mailgun.com", true)
-	require.NoError(t, err)
 }
