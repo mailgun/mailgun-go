8c822bf3fc8c8d7b166459beaac5a2f44105cf6e
Vilen Topchii <32271530+vtopc@users.noreply.github.com>
2026-08-15 18:50:34 +0200

    removed redundant

--- mocks/mock_api_keys.go
+++ mocks/mock_api_keys.go
@@ -25,12 +25,6 @@ func (ms *Server) listAPIKeys(w http.ResponseWriter, _ *http.Request) {
 }
 
 func (ms *Server) createAPIKey(w http.ResponseWriter, r *http.Request) {
-	err := r.ParseForm()
-	if err != nil {
-		http.Error(w, "Failed to parse form data", http.StatusBadRequest)
-		return
-	}
-
 	resp := mtypes.CreateAPIKeyResponse{
 		Key: mtypes.APIKey{ID: "1", Role: r.FormValue("role")},
 	}
