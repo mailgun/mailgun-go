21e77b1402c07db765cd4856fa52b6b3f7d173b5
Vilen Topchii <32271530+vtopc@users.noreply.github.com>
2026-08-15 19:04:25 +0200

    AddBounce TODO

--- bounces.go
+++ bounces.go
@@ -157,7 +157,7 @@ func (mg *Client) AddBounce(ctx context.Context, domain, address, code, bounceEr
 	r.setClient(mg.HTTPClient())
 	r.setBasicAuth(basicAuthUser, mg.APIKey())
 
-	// TODO(vtopc): should be "application/json" instead of "application/x-www-form-urlencoded" based on docs:
+	// TODO(vtopc): should be "application/json" or form-data instead of "application/x-www-form-urlencoded" based on docs:
 	// https://documentation.mailgun.com/docs/inboxready/api-reference/optimize/mailgun/bounces/post-v3--domainid--bounces
 	payload := newUrlEncodedPayload()
 	payload.addValue("address", address)
