e5b013f6a6c853d00f5a5970dea99f64bc6b9901
Vilen Topchii <32271530+vtopc@users.noreply.github.com>
2026-09-06 16:17:26 +0200

    rename to ListDomainsIter

--- domains.go
+++ domains.go
@@ -28,7 +28,7 @@ type ListDomainsOptions struct {
 	IncludeSubaccounts *bool
 }
 
-// ListDomains returns an iterator over pages of domains.
+// ListDomainsIter returns an iterator over pages of domains.
 // Iteration stops after the first error, which is yielded with a nil page.
 // https://documentation.mailgun.com/docs/mailgun/api-reference/send/mailgun/domains/get-v4-domains
 func (mg *Client) ListDomainsIter(ctx context.Context, opts *ListDomainsOptions) iter.Seq2[[]mtypes.Domain, error] {
