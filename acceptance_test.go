f07323ad345c6a85ea60337580304d45573d7ba9
Vilen Topchii <32271530+vtopc@users.noreply.github.com>
2026-09-06 17:45:36 +0200

    added TODO

--- acceptance_test.go
+++ acceptance_test.go
@@ -6,6 +6,7 @@ import (
 )
 
 // Return the variable missing which caused the test to be skipped
+// TODO(vtopc): merge with `//go:build integration`
 func SkipNetworkTest() string {
 	for _, env := range []string{"MG_DOMAIN", "MG_API_KEY", "MG_EMAIL_TO"} {
 		if os.Getenv(env) == "" {
